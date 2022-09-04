4bf3e60bbfff6c5f3e04b94513ae9db1c899d4b3
MiracleWong <cfwr1991@126.com>
2022-09-04 18:16:23 +0800

    add brew.sh & install apps qlapps caskapps

--- script/install/macos/brew.sh
+++ script/install/macos/brew.sh
@@ -10,6 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
+  # 
   git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
   /bin/bash brew-install/install.sh
   rm -rf brew-install
@@ -19,7 +20,94 @@ if test ! $(which brew); then
   for tap in core cask{,-fonts,-drivers,-versions} command-not-found; do
       brew tap --custom-remote --force-auto-update "homebrew/${tap}" "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git"
   done
+  brew update
+  brew upgrade
 fi
 
-brew update
-brew upgrade
+# brew update
+# brew upgrade
+echo "brew update & upgrade OK"
+
+# Install packages
+apps=(
+  python@3.10
+  coreutils
+  tree
+  wget
+  curl
+  mycil
+  httpie
+  lftp
+  lrzsz
+  jq
+  xz
+  coreutils
+  autojump
+  shellcheck
+  htop
+  axel
+  cloc
+  thefuck
+  wtf
+  fzf
+  exa
+  bat
+  tmux
+  gh
+  imagemagick
+  restic
+)
+
+brew install "${apps[@]}"
+
+qlapps=(
+  qlcolorcode
+  qlimagesize
+  qlmarkdown
+  qlmobi
+  qlstephen
+  qlvideo
+  quicklook-json
+  quicklook-csv
+  webpquicklook
+  epubquicklook
+  ipynb-quicklook
+  provisionql
+)
+
+brew install --cask "${qlapps[@]}"
+
+caskapps=(
+  appcleaner
+  alfred
+  iterm2
+  sogouinput
+  google-chrome
+  neovim
+  visual-studio-code
+  useage
+  switchhosts
+  # tencent-lemon
+  # wechat
+  # qq
+  # qqmusic
+  # dash
+  # iina
+  # flux
+  # licecap
+  # maczip
+  # hiddenbar
+  # obsidian
+  # itsycal
+  # hazeover
+  # sourcetree
+  # picgo
+  # one-switch # 付费
+  # mweb-pro # 付费
+)
+
+brew install --cask "${caskapps[@]}"
+
+brew cleanup
+
+open /Applications/Alfred*.app
